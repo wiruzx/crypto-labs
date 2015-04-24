f2982b4e603679ec9bdd43785c1ab2eff9e08acf
Victor Shamanov <wiruzx@gmail.com>
2015-04-24 18:43:56 +0300

    Initial commit

---
+++ src/crypto_labs/core.clj
@@ -0,0 +1,6 @@
+(ns crypto-labs.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/crypto_labs/core_test.clj
@@ -0,0 +1,7 @@
+(ns crypto-labs.core-test
+  (:require [clojure.test :refer :all]
+            [crypto-labs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
